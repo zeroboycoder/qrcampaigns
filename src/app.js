d3be11b704cebaa5a7a6bbe3a44063d023edd78b
zeroboycoder <pyaesonekhant@tumawlamyine.edu.mm>
2022-03-16 17:06:16 +0630

    Finish Campaigns Page

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./containers/Home/Home";
+import Campaigns from "./containers/campaigns/campaigns";
+import Contact from "./containers/contact/contact";
 import Pnf from "./containers/pnf/pnf";
 import Layout from "./hoc/Layout";
 
@@ -12,6 +14,8 @@ const app = () => {
     <Layout>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/campaigns" element={<Campaigns />} />
+        <Route exact path="/contact" element={<Contact />} />
         <Route path="*" element={<Pnf />} />
       </Routes>
     </Layout>
---
+++ src/containers/campaigns/campaigns.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import HoriBg from "../../assets/img/horiBg.png";
+import QRone from "../../assets/img/qr1.png";
+import QRtwo from "../../assets/img/qr2.png";
+
+import "./campaigns.css";
+
+const Campaigns = () => {
+  return (
+    <div className="campaigns">
+      <div className="horiBg"></div>
+      <div className="title">
+        <h2>KYAW MOBILE MAWLAMYINE မှနွေးထွေးစွာကြိုဆိုပါသည်။</h2>
+      </div>
+      <div className="campaignsBody">
+        <div className="campaignDescBox">
+          <div className="descBox">
+            <h2>QR SCAN ဖတ်ရာတွင်လုပ်ဆောင်ရမည့်အချက်များ</h2>
+            <p>
+              မိမိဖုန်းထဲရှိ camera app ကိုဖွင့်ပါ။ <br /> ဝယ်ယူထားသည့်
+              ပစ္စည်းတွင်ပါဝင်သော <br /> QR Code ကို အထက်တွင်ဖော်ပြခဲ့သော <br />{" "}
+              Camera App ဖြင့် Scan ဖတ်ပါ။
+            </p>
+            <div className="btnContainer">
+              <button>OPEN CAMERA</button>
+              <button>SCAN NOW</button>
+            </div>
+          </div>
+        </div>
+        <div className="campaignImg">
+          <div className="imgContainerOne">
+            <img src={QRone} alt="QR" className="img" />
+          </div>
+          <div className="imgContainerTwo">
+            <img src={QRtwo} alt="QR" className="img" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Campaigns;
