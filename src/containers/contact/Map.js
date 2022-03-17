80a9adfe24e7248b6b3fe43ad29773d9db3bfc47
zeroboycoder <pyaesonekhant@tumawlamyine.edu.mm>
2022-03-17 17:59:30 +0630

    Finish Contact Page

---
+++ src/containers/contact/Map.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Map = () => {
+  return (
+    <iframe
+      width="100%"
+      height="100%"
+      frameborder="0"
+      scrolling="no"
+      marginheight="0"
+      marginwidth="0"
+      src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Kyaw%20Mobile,%20No.212,%20Shwedaung%20Qtr,%20Lower%20Main%20Rd,%20Mawlamyine+(Kyaw%20Mobile%20Mawlamyine)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+    >
+      <a href="https://www.gps.ie/marine-gps/">navigation gps</a>
+    </iframe>
+  );
+};
+
+export default Map;
--- src/containers/contact/contact.js
+++ src/containers/contact/contact.js
@@ -1,7 +1,81 @@
 import React from "react";
+import {
+  PhoneRounded,
+  LanguageRounded,
+  FacebookRounded,
+  YouTube,
+  Language,
+  Telegram,
+} from "@mui/icons-material";
+
+import Map from "./Map";
+import MapWithPhone from "../../assets/img/mapwithphone.png";
+import "./contact.css";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="Contact">
+      <div className="HoriBg"></div>
+      <div className="upperSection">
+        <div className="mapContainer">
+          <Map />
+        </div>
+        <div className="imgContainer">
+          <img src={MapWithPhone} alt="Map with phone image" className="img" />
+        </div>
+      </div>
+      <div className="contactFooter">
+        {/* First Footer */}
+        <div className="footerOne">
+          <div className="middle">
+            <div className="contactBox">
+              <div className="iconContainer iconContainerOne">
+                <PhoneRounded style={{ fontSize: "24px", color: "#fff" }} />
+              </div>
+              <div className="contactInfo">
+                <p>Contact Company</p>
+                <p>09 775-321-446</p>
+              </div>
+            </div>
+            <div className="contactBox">
+              <div className="iconContainer iconContainerTwo">
+                <LanguageRounded style={{ fontSize: "24px", color: "#fff" }} />
+              </div>
+              <div className="contactInfo">
+                <p>Website Company</p>
+                <p>https://kyawmobilemawlamyine.com</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Second Footer */}
+        <div className="contactChannel">
+          <div className="box">Our Channel</div>
+          <div className="contact icons">
+            <a
+              href="https://www.facebook.com/kyawmobilemawlamyine"
+              target="_blank"
+            >
+              <FacebookRounded className="fb" style={{ fontSize: "26px" }} />
+            </a>
+            <a
+              href="https://www.youtube.com/c/KyawMobileMalamyine"
+              target="_blank"
+            >
+              <YouTube className="yt" style={{ fontSize: "26px" }} />
+            </a>
+            <a href="https://kyawmobilemawlamyine.com" target="_blank">
+              <Language className="web" style={{ fontSize: "26px" }} />
+            </a>
+            <a href="https://t.me/kyawmobilemawlamyine" target="_blank">
+              <Telegram className="tg" style={{ fontSize: "26px" }} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
