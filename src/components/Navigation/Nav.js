000ada3361e49a067ee63eede64d56cd52ce7f2c
zeroboycoder <pyaesonekhant@tumawlamyine.edu.mm>
2022-03-20 17:53:17 +0630

    Almost Finish UI

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -1,31 +1,43 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { MenuRounded } from "@mui/icons-material";
 import "./Nav.css";
 
 const Nav = () => {
   return (
-    <div className="Nav">
-      <NavLink
-        to="/"
-        className={(active) => (active.isActive ? "navLink active" : "navLink")}
-      >
-        Home
-      </NavLink>
-      <NavLink
-        to="/campaigns"
-        className={(active) => (active.isActive ? "navLink active" : "navLink")}
-      >
-        {" "}
-        Campaigns
-      </NavLink>
-      <NavLink
-        to="/contact"
-        className={(active) => (active.isActive ? "navLink active" : "navLink")}
-      >
-        {" "}
-        Contact
-      </NavLink>
-    </div>
+    <>
+      <div className="Nav">
+        <NavLink
+          to="/"
+          className={(active) =>
+            active.isActive ? "navLink active" : "navLink"
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/campaigns"
+          className={(active) =>
+            active.isActive ? "navLink active" : "navLink"
+          }
+        >
+          {" "}
+          Campaigns
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={(active) =>
+            active.isActive ? "navLink active" : "navLink"
+          }
+        >
+          {" "}
+          Contact
+        </NavLink>
+      </div>
+      <div className="hamburgerMenu">
+        <MenuRounded fontSize="medium" style={{ color: "var(--red)" }} />
+      </div>
+    </>
   );
 };
 
