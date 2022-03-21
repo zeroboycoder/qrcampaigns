09d59748b4c97809ed53fb5309f6992a1bcce546
zeroboycoder <pyaesonekhant@tumawlamyine.edu.mm>
2022-03-21 16:57:45 +0630

    Starting prepare

--- src/app.js
+++ src/app.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./containers/Home/Home";
 import Campaigns from "./containers/campaigns/campaigns";
 import Gift from "./containers/campaigns/gift/gift";
+import Prepare from "./containers/campaigns/prepare/prepare";
 import Contact from "./containers/contact/contact";
 import Pnf from "./containers/pnf/pnf";
 import Layout from "./hoc/Layout";
 
 import "./app.css";
 
-const app = () => {
+const App = () => {
+  const [prepare, setPrepare] = useState();
+
+  const checkPrepare = () => {
+    return true;
+  };
   return (
     <Layout>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/campaigns/:qrcode" element={<Gift />} />
+        <Route
+          path="/campaigns/:qrcode"
+          element={checkPrepare() ? <Prepare /> : <Gift />}
+        />
         <Route path="/campaigns" element={<Campaigns />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<Pnf />} />
@@ -24,4 +33,4 @@ const app = () => {
   );
 };
 
-export default app;
+export default App;
---
+++ src/containers/campaigns/prepare/prepare.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./prepare.css";
+
+const Prepare = () => {
+  return <p>Prepare</p>;
+};
+
+export default Prepare;
